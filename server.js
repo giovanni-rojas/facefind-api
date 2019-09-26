316204bf69fd263d0223137e85d89d48425dddcc
giovanni-rojas <gio.rojas15@gmail.com>
2019-09-25 21:18:23 -0700

    connected backend to local postgress database using knex. seems to be successful...

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const knex = require('knex');
+
+const postgres = knex(
+	{
+	  client: 'pg',
+	  connection: 
+		  {
+		    host : '127.0.0.1',
+		    user : 'postgres',
+		    password : '',
+		    database : 'facefind'
+		  }
+	}
+);
+
+//console.log(postgres.select('*').from('users'));
 
 const app = express();
 
