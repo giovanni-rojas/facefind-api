75ea42f9d6785336640e6c24046f95998270f675
giovanni-rojas <gio.rojas15@gmail.com>
2019-09-26 20:39:19 -0700

    updated /signin, correctly checks credentials

--- server.js
+++ server.js
@@ -60,20 +60,23 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-	bcrypt.compare("dope", '$2b$10$E7OLTpP.O2CalrU/6Jym3eFuOppgJBEocsFkBzYriXzMc60Nx4icW')
-		.then(function(res) {
-			console.log('first guess: ', res)
-		});
-
-	bcrypt.compare("veggies", '$2b$10$E7OLTpP.O2CalrU/6Jym3eFuOppgJBEocsFkBzYriXzMc60Nx4icW')
-		.then(function(res) {
-			console.log('second guess: ', res)
-		});
-	if (req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password) 
-		res.json(database.users[0]);
-	else
-		res.status(400).json('error logging in');
+	const { email, password } = req.body;
+	db.select('email', 'hash').from('login')
+		.where('email', '=', email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(password, data[0].hash);
+			if (isValid) {
+				return db.select('*').from('users')
+					.where('email', '=', email)
+					.then(user => {
+						res.json(user[0]);
+					})
+					.catch(err => res.status(400).json('unable to get user'))
+			}
+			else
+				res.status(400).json('wrong credentials')
+		})
+		.catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
