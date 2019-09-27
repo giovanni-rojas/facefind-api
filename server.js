a5fa9d63b3a270af78f13085b208f438387a76e9
giovanni-rojas <gio.rojas15@gmail.com>
2019-09-26 20:11:35 -0700

    modified /register, now adding users to both 'users' and 'login' tables

--- server.js
+++ server.js
@@ -78,17 +78,35 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
-	db('users')
-		.returning('*')		//returns all columns
-		.insert({			//inserts into db, only columns we need
-		email: email,
-		name: name,
-		joined: new Date()
+	
+	//hash passwords
+	const salt = bcrypt.genSaltSync(saltRounds);
+	const hash = bcrypt.hashSync(password, salt);
+	
+	db.transaction(trx => {		//used when modifying multiple tables. If modifying one fails, they all fail
+		trx.insert({			//insert user's login info to 'login' table
+			email: email,
+			hash: hash
 		})
-		.then(user => {
-			res.json(user[0]);
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('users')
+			.returning('*')		//returns all columns
+			.insert({			//inserts into db, only columns we need
+			email: loginEmail[0],
+			name: name,
+			joined: new Date()
+			})
+			.then(user => {
+				res.json(user[0]);
+			})
 		})
-		//.catch(err => res.status(400).json(err)); 	//this would return actual user info. Not good!
+		.then(trx.commit)
+		.catch(trx.rollback)
+	})	
+
+	 	//this would return actual user info. Not good!
 		.catch(err => res.status(400).json('unable to return user'));
 })
 
