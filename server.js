1a593064cc0d04079e8e6f34c05b97bd27da61c3
Gio Rojas <gio.rojas15@gmail.com>
2024-07-23 15:21:21 -0700

    final test?

--- server.js
+++ server.js
@@ -12,6 +12,27 @@ const image = require('./controllers/image');
 
 const PORT = process.env.PORT;
 
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'Sally',
+			email: 'sally@gmail.com',
+			password: 'bananas',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+}
+
 const db = knex(
 	{
 	  client: 'pg',
@@ -30,8 +51,33 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => { res.send("it's working!") })
+
 app.post('/signin', (req, res) => { signin.handleSignin(db, bcrypt) });												//cleaner way of running this, but a bit confusing to grasp
+
+// app.post('/signin', (req, res) => {
+// 	if (req.body.email === database.users[0].email && 
+// 		req.body.password === database.users[0].password) {
+// 		res.json('success');
+// 	}
+// 	else {
+// 		res.status(400).json('error logging in');
+// 	}
+// })
+
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });		//dependency injection important
+// app.post('/register', (req, res) => {
+// 	const { email, name, password } = req.body;
+// 	database.users.push({
+// 		id: '125',
+// 		name: name,
+// 		email: email,
+// 		password: password,
+// 		entries: 0,
+// 		joined: new Date()
+// 	})
+// 	res.json(database.users[database.users.length - 1]);
+// })
+
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
