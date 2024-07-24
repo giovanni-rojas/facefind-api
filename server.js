5e1fe784e2c0d6bfcfe670b680ba21bbe62c0b78
Gio Rojas <gio.rojas15@gmail.com>
2024-07-23 17:49:02 -0700

    todo: clarifai update

--- server.js
+++ server.js
@@ -2,82 +2,47 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-//const saltRounds = 10;
 const knex = require('knex');
-
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const PORT = process.env.PORT;
-
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'sally@gmail.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		}
-	]
-}
-
-const db = knex(
-	{
-	  client: 'pg',
-	  connection: 
-		  {
-			connectionString : 'process.env.DATABASE_URL',
-			ssl: true
-		  }
+const db = knex({
+	client: 'pg',
+	connection: {
+		host : '127.0.0.1',
+		user : 'postgres',
+		password : 'test73$7',
+		database : 'postgres'
 	}
-);
+});
+
+// const db = knex(
+// 	{
+// 	  client: 'pg',
+// 	  connection: 
+// 		  {
+// 			connectionString : 'process.env.DATABASE_URL',
+// 			ssl: true
+// 		  }
+// 	}
+// );
+
+db.select('*').from('users').then(data => {
+	console.log(data);
+});
+
+const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => { res.send("it's working!") })
-
-app.post('/signin', (req, res) => { signin.handleSignin(db, bcrypt) });												//cleaner way of running this, but a bit confusing to grasp
-
-// app.post('/signin', (req, res) => {
-// 	if (req.body.email === database.users[0].email && 
-// 		req.body.password === database.users[0].password) {
-// 		res.json('success');
-// 	}
-// 	else {
-// 		res.status(400).json('error logging in');
-// 	}
-// })
-
+app.get('/', (req, res) => { res.send('success') })
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });												//cleaner way of running this, but a bit confusing to grasp
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });		//dependency injection important
-// app.post('/register', (req, res) => {
-// 	const { email, name, password } = req.body;
-// 	database.users.push({
-// 		id: '125',
-// 		name: name,
-// 		email: email,
-// 		password: password,
-// 		entries: 0,
-// 		joined: new Date()
-// 	})
-// 	res.json(database.users[database.users.length - 1]);
-// })
-
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
