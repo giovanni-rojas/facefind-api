73c122caa763b4a1de939fa10ba1cfb27c27ca5f
Gio Rojas <gio.rojas15@gmail.com>
2024-09-19 21:50:43 -0700

    heroku testt

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ const PORT = process.env.PORT
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.get('/', (req, res) => { res.send('success') })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
